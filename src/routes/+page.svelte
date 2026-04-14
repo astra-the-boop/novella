dc256e94349c14808c825b9fa0dbd635fc5d9a02
maxstellar <93678379+maxstellar@users.noreply.github.com>
2026-04-13 21:47:08 -0700

    feat: added a note from the designers

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+	import { resolve } from '$app/paths';
 	import { onMount } from 'svelte';
 	import { fade } from 'svelte/transition';
 	import { bottomCarouselImages, topCarouselImages } from '$lib/carousel';
 	import './page.css';
+
+	const novella = resolve('/novella');
 
 	const DUE_YEAR: number = 2026;
 	const DUE_MONTH: number = 4;
@@ -747,7 +750,7 @@
 			>
 		</div>
 		<div class="button">
-			<a id="linkButton">A note from the designers ↗</a>
+			<a id="linkButton" href={novella}>A note from the designers ↗</a>
 		</div>
 	</div>
 </section>
